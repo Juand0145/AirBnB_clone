ce5032f8767da315ec91eef62a1e3f5665211c14
Juand0145 <juand0145@gmail.com>
2021-06-25 10:32:33 -0500

    Fixing task 4 and adding task 5

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,34 @@
 ''' '''
 import uuid
 import datetime
+import models
+
 
 class BaseModel():
     ''' '''
+
     def __init__(self, *args, **kwargs):
         ''' '''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key != "__class__":
                     self.key = value
 
-            if "created_at" in kwargs():
-                self.created_at = datetime.strptime(self.created_at)
-                self.updated_at = datetime.strptime(self.created_at)
+            if "created_at" in kwargs:
+                self.created_at = datetime.datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
 
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-                      
+            models.storage.new(self)
+            models.storage.save()
+
     def __str__(self):
         ''' '''
         return "[{}] ({}) {}". format(__class__.__name__, self.id, self.__dict__)
@@ -28,6 +37,7 @@ class BaseModel():
     def save(self):
         ''' '''
         self.updated_at = datetime.datetime.now()
+        models.storage.save
 
     def to_dict(self):
         ''' '''
@@ -36,5 +46,3 @@ class BaseModel():
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
-
-    
---
+++ models/engine/file_storage.py
@@ -0,0 +1,31 @@
+#!/usr/Bin/python3
+''' '''
+import json
+from models.base_model import BaseModel
+
+class FileStorage():
+    ''' '''
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        ''' '''
+        return self.__objects
+
+    def new(self, obj):
+        ''' '''
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        ''' '''
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key]
+        with open(self.__file_path, 'w') as file:
+            json.dump(json_objects, file)
+
+    def reload(self):
+        ''' '''
+    
