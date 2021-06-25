87067b184180f86e54251f6887840c59282c67be
Juand0145 <juand0145@gmail.com>
2021-06-24 19:15:19 -0500

    Adding task 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,22 @@ import datetime
 
 class BaseModel():
     ''' '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         ''' '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
 
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    self.key = value
+
+            if "created_at" in kwargs():
+                self.created_at = datetime.strptime(self.created_at)
+                self.updated_at = datetime.strptime(self.created_at)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+                      
     def __str__(self):
         ''' '''
         return "[{}] ({}) {}". format(__class__.__name__, self.id, self.__dict__)
@@ -27,3 +37,4 @@ class BaseModel():
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
 
+    
