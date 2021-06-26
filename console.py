2b43d22a6c970704b85678f040ec6db9fb8efe8e
Adriana Echeverri <adriana.er98@hotmail.com>
2021-06-26 16:28:28 -0500

    added (update and all) features to the console

--- console.py
+++ console.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 ''' '''
 import cmd
-import json
 import models
 from models.base_model import BaseModel
 from models import storage
 
 classes = {'BaseModel': BaseModel()}
+
 
 class HBNBCommand(cmd.Cmd):
     ''' '''
@@ -22,7 +22,7 @@ class HBNBCommand(cmd.Cmd):
         if arg is not None:
             exit()
 
-    def verification(self, arguments):
+    def verification(self, arguments, mode):
 
         commands = arguments.split()
         if len(commands) == 0:
@@ -33,31 +33,35 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return None
 
+        elif mode == 1:
+            if len(commands) < 2:
+                print("** instance id missing **")
+                return None
+
         return commands
 
     def do_create(self, arg):
         ''' '''
-        commands = self.verification(arg)
+        commands = self.verification(arg, 0)
 
-        if commands is not None:
-            instance = classes[commands[0]]
-            instance.save()
-            print(instance.id)
-    
-    def do_show(self, arg):
-        ''' '''
-        commands = self.verification(arg)
-        
         if commands is None:
             return
 
-        if len(commands) < 2:
-            print("** instance id missing **")
+        instance = classes[commands[0]]
+        storage.new(instance)
+        storage.save()
+        print(instance.id)
+
+    def do_show(self, arg):
+        ''' '''
+        commands = self.verification(arg, 1)
+
+        if commands is None:
             return
 
         key = "{}.{}".format(commands[0], commands[1])
         instances_dict = models.storage.all()
-        
+
         if key not in instances_dict.keys():
             print("** no instance found **")
 
@@ -66,13 +70,9 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, arg):
         ''' '''
-        commands = self.verification(arg)
+        commands = self.verification(arg, 1)
 
         if commands is None:
-            return
-
-        if len(commands) < 2:
-            print("** instance id missing **")
             return
 
         key = "{}.{}".format(commands[0], commands[1])
@@ -87,16 +87,70 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         ''' '''
-        commands = arg.split()
+        classes_to_print = arg.split()
         instances_dict = models.storage.all()
         instances_list = []
 
-        if commands not in classes.keys():
-            print("** class doesn't exist **")
-            return None
-        
+        for cls in classes_to_print:
+            if cls not in classes.keys():
+                print("** class doesn't exist **")
+                return None
+
         for object in instances_dict.values():
-            instances_list.append(object.__str__)
+            if len(classes_to_print) > 0:
+                for cls in classes.values():
+                    if type(object) == type(cls):
+                        instances_list.append(object.__str__())
+            else:
+                instances_list.append(object.__str__())
+
+        print(instances_list)
+
+    def do_update(self, arg):
+        ''' '''
+        commands = self.verification(arg, 1)
+        if commands is None:
+            return
+
+        key = "{}.{}".format(commands[0], commands[1])
+        instances_dict = models.storage.all()
+
+        if key not in instances_dict.keys():
+            print("** no instance found **")
+
+        else:
+            if len(commands) < 3:
+                print("** attribute name missing **")
+                return
+
+            if len(commands) < 4:
+                print("** value missing **")
+                return
+
+            object_to_update = instances_dict[key]
+            attribute = commands[2]
+            new_value = commands[3]
+            casted_value = cast_variable(new_value, type(getattr(object_to_update, attribute)))
+            setattr(object_to_update, attribute, casted_value)
+            object_to_update.save()
+
+
+def cast_variable(variable, new_type):
+    ''' '''
+    if new_type == int:
+        return int(variable)
+
+    elif new_type == float:
+        return float(variable)
+
+    elif new_type == list:
+        return list(variable)
+
+    elif new_type == dict:
+        return dict(variable)
+
+    return variable
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
