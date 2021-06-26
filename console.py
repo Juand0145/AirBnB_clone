9d2fafe301cb2a0ec6bce0631c6fcf2ba2652e47
Juand0145 <juand0145@gmail.com>
2021-06-26 14:08:52 -0500

    Fixing task 7

--- console.py
+++ console.py
@@ -2,7 +2,9 @@
 ''' '''
 import cmd
 import json
+import models
 from models.base_model import BaseModel
+from models import storage
 
 classes = {'BaseModel': BaseModel()}
 
@@ -35,7 +37,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         ''' '''
-        commands = self.verification(self, arg)
+        commands = self.verification(arg)
 
         if commands is not None:
             instance = classes[commands[0]]
@@ -44,13 +46,57 @@ class HBNBCommand(cmd.Cmd):
     
     def do_show(self, arg):
         ''' '''
-        commands = self.verification(self, arg)
+        commands = self.verification(arg)
         
-        if len(commands) < 3:
+        if commands is None:
+            return
+
+        if len(commands) < 2:
             print("** instance id missing **")
+            return
 
+        key = "{}.{}".format(commands[0], commands[1])
+        instances_dict = models.storage.all()
+        
+        if key not in instances_dict.keys():
+            print("** no instance found **")
 
-    
+        else:
+            print(instances_dict[key])
+
+    def do_destroy(self, arg):
+        ''' '''
+        commands = self.verification(arg)
+
+        if commands is None:
+            return
+
+        if len(commands) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(commands[0], commands[1])
+        instances_dict = models.storage.all()
+
+        if key not in instances_dict.keys():
+            print("** no instance found **")
+
+        else:
+            instances_dict.pop(key)
+            models.storage.save()
+
+    def do_all(self, arg):
+        ''' '''
+        commands = arg.split()
+        instances_dict = models.storage.all()
+        instances_list = []
+
+        if commands not in classes.keys():
+            print("** class doesn't exist **")
+            return None
+        
+        for object in instances_dict.values():
+            instances_list.append(object.__str__)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
