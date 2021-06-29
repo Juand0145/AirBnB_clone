9e6210bcc61c5117143605f0390c55a6474b7097
Juand0145 <juand0145@gmail.com>
2021-06-29 16:38:33 -0500

    Adding file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''Testing file for the class BaseModel'''
+'''Testing file for the class FileStorage'''
 import unittest
+from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
