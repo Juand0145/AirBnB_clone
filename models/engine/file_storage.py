4659c5b37cebe8c9f7bdf6190a2fe46f908d72af
Juand0145 <juand0145@gmail.com>
2021-06-29 15:22:18 -0500

    Changing file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,7 @@ to a JSON file and deserializes JSON file to instances:'''
 import json
 
 
-class FileStorage():
+class FileStorage:
     '''Is a class FileStorage that serializes instances
     to a JSON file and deserializes JSON file to instances:'''
     __file_path = "file.json"
