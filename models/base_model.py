6f4460274c7f55ebeae8a13e3e16e3cdcec42c2a
Adriana Echeverri <adriana.er98@hotmail.com>
2021-06-29 17:19:57 -0500

    Change updated at to be equal to created

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.updated_at = self.created_at()
             storage.new(self)
 
     def __str__(self):
