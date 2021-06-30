632623fbc8ffacad4332deb8f3e73046c3fbf427
Adriana Echeverri <adriana.er98@hotmail.com>
2021-06-30 13:29:34 -0500

    add testing for file storage methods

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Testing file for the class FileStorage"""
+from typing import Type
 import unittest
+import json
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
@@ -20,19 +22,65 @@ class TestFileStorage(unittest.TestCase):
 
     def test_new(self):
         '''Testing function to check the instance method new'''
-        # file = FileStorage()
-        # object_1 = file.all()
-
-        # test = BaseModel()
-        # file.new(test)
-        # object_2 = file.all()
-        # self.assertNotEqual(object_1, object_2)
-        pass
+        file = FileStorage()
+        dictionary = {"id": "64a8e9f5-4840-431c-9305-baff80edd778",
+                      "created_at": "2021-06-30T10:45:57.274432",
+                      "updated_at": "2021-06-30T10:45:57.274432"}
+        instance = BaseModel(**dictionary)
+        file.new(instance)
+        objects = file.all()
+        self.assertTrue(
+            "BaseModel.64a8e9f5-4840-431c-9305-baff80edd778" in objects.keys())
 
     def test_save(self):
         '''Testing function to check the instance method save'''
-        self.assertEqual(self.storage.save(), None)
+        dictionary = {"id": "64a8e9f5-4840-431c-9305-baff80edd778",
+                      "created_at": "2021-06-30T10:45:57.274432",
+                      "updated_at": "2021-06-30T10:45:57.274432"}
+        dictionary_2 = {"id": "64a8e9f5-4840-431c-9305-baff80edd779",
+                        "created_at": "2021-06-30T10:45:57.274438",
+                        "updated_at": "2021-06-30T10:45:57.274438"}
+        instance_1 = BaseModel(**dictionary)
+        self.storage.new(instance_1)
+        instance_2 = BaseModel(**dictionary_2)
+        self.storage.new(instance_2)
+        self.storage.save()
+        with open('file.json', 'r', encoding='utf-8') as myfile:
+            file_content = json.load(myfile)
+            self.assertTrue(
+                "BaseModel.64a8e9f5-4840-431c-9305-baff80edd778",
+                file_content.keys())
+            self.assertTrue(
+                "BaseModel.64a8e9f5-4840-431c-9305-baff80edd779",
+                file_content.keys())
 
     def test_reload(self):
         '''Testing function to check the instance method reload'''
-        pass
+        with open('file.json', 'w', encoding='utf-8') as myfile:
+            class_write = "{\"BaseModel.7c1ef07d-5b19-42a0-85d0-67b3ec96\": \
+                            {\"id\": \"7c1ef07d-5b19-42a0-85d0-67b3ec96\", \
+                            \"created_at\": \"2021-06-30T10: 51: 30.849940\", \
+                            \"updated_at\": \"2021-06-30T10: 51: 30.849940\", \
+                            \"__class__\": \"BaseModel\"}}"
+            myfile.write(class_write)
+            self.storage.reload()
+            object = self.storage.all()
+            self.assertTrue(
+                "BaseModel.7c1ef07d-5b19-42a0-85d0-67b3ec96a50f",
+                object.keys())
+
+    def test_exceding_args(self):
+        '''Testing function to check methods with more arguments'''
+        dictionary = {"id": "64a8e9f5-4840-431c-9305-baff80edd778",
+                      "created_at": "2021-06-30T10:45:57.274432",
+                      "updated_at": "2021-06-30T10:45:57.274432",
+                      "__class__": "BaseModel"}
+        test = BaseModel(**dictionary)
+        with self.assertRaises(TypeError):
+            self.storage.new(test, test)
+
+        with self.assertRaises(TypeError):
+            self.storage.save(test)
+
+        with self.assertRaises(TypeError):
+            self.storage.reload(test)
